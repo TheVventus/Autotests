6f09bc06382c1258bc20c6a9d999212d78b74ee1
Ильдар <thevventus@gmail.com>
2021-05-07 12:25:15 +0400

    Fix: small fixes

---
+++ src/pages/home/mainMenu/mainMenu.php
@@ -0,0 +1,10 @@
+<?php
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use \Facebook\WebDriver\WebDriverKeys;
+use Facebook\WebDriver\WebDriverBy;
+
+class MainMenu {
+
+    private $driver;
+    
+}
--- tests/home/homeTest.php
+++ tests/home/homeTest.php
@@ -1,27 +1,41 @@
 <?php
 
-require 'tests/baseTest.php';
+include 'helpers/baseTest.php';
+include 'src/pages/home/homePage.php';
 use Facebook\WebDriver\WebDriverBy;
 
 class HomeTest extends BaseTest {
 
-    /**
-      * @test
-      */
+    /** @test */
     public function test_searchTextOnGoogle() {
-        $this->driver->get("https://www.google.com/ncr");
-        $this->driver->manage()->window()->maximize();
-        
-        sleep(5);
-        
-        $element = $this->driver->findElement(WebDriverBy::name("q"));
-        if($element) {
-          $element->sendKeys("LambdaTest");
-          $element->submit();
-        }
-        
-        print $this->driver->getTitle();
-        $this->assertEquals('LambdaTest - Google Search', $this->driver->getTitle());
+      $this->driver->get("https://www.google.com/ncr");
+      $this->driver->manage()->window()->maximize();
+      
+      sleep(5);
+      
+      $element = $this->driver->findElement(WebDriverBy::name("q"));
+      if($element) {
+        $element->sendKeys("LambdaTest");
+        $element->submit();
       }
+      
+      print $this->driver->getTitle();
+      $this->assertEquals('LambdaTest - Google Search', $this->driver->getTitle());
+    }
+
+    /** @test */
+    public function testGoogleHome()
+    {
+        $this->googlesearchpage->openURL();
+        $this->assertEquals('Google', $this->googlesearchpage->title());
+    }
+
+    /** @test */
+    public function testGoogleSearch()
+    {
+        $this->googlesearchpage->openURL();
+        $this->googlesearchpage->searchFor('Selenium');
+        $this->assertTrue($this->searchresultspage->isSeleniumResultPresent(),'Selenium Result Found');
+    }
 
 }
